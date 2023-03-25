249694244288c96dab883a46d873c7056d664a9f
rucha-kulkarni <rucha3702@gmail.com>
2023-03-25 20:01:33 +0530

    parkplus

---
+++ src/main/java/com/driver/model/Payment.java
@@ -0,0 +1,62 @@
+package com.driver.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Payment {
+    int id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMode paymentMode;
+
+    private Boolean paymentCompleted;
+
+    @OneToOne
+    @JoinColumn
+    @JsonIgnore
+    Reservation reservation;
+
+    public Payment() {
+    }
+
+    public Payment(int id, Boolean paymentCompleted, Reservation reservation) {
+        this.id = id;
+        this.paymentCompleted = paymentCompleted;
+        this.reservation = reservation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public PaymentMode getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(PaymentMode paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+
+    public Boolean getPaymentCompleted() {
+        return paymentCompleted;
+    }
+
+    public void setPaymentCompleted(Boolean paymentCompleted) {
+        this.paymentCompleted = paymentCompleted;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+}
--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -1,5 +1,7 @@
 package com.driver.services.impl;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
 import com.driver.model.SpotType;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
@@ -7,8 +9,8 @@ import com.driver.services.ParkingLotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class ParkingLotServiceImpl implements ParkingLotService {
@@ -18,26 +20,56 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     SpotRepository spotRepository1;
     @Override
     public ParkingLot addParkingLot(String name, String address) {
-
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setName(name);
+        parkingLot.setAddress(address);
+        parkingLotRepository1.save(parkingLot);
+        return parkingLot;
     }
 
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
 
+        Spot spot = new Spot(pricePerHour);
+        if(numberOfWheels>4){
+            spot.setSpotType(SpotType.OTHERS);
+        }else if(numberOfWheels>2){
+            spot.setSpotType(SpotType.FOUR_WHEELER);
+        }else
+            spot.setSpotType(SpotType.TWO_WHEELER);
+
+        spot.setOccupied(true);
+        parkingLot.getSpotList().add(spot);
+        spot.setParkingLot(parkingLot);
+        parkingLotRepository1.save(parkingLot);
+        return spot;
     }
 
     @Override
     public void deleteSpot(int spotId) {
-
+         spotRepository1.deleteById(spotId);
     }
 
     @Override
     public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
-
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        Spot spot = null;
+        List<Spot> spotList = parkingLot.getSpotList();
+        for(Spot spot1 : spotList){
+            if(spot1.getId()== spotId){
+                spot=spot1;
+                break;
+            }
+        }
+        spot.setPricePerHour(pricePerHour);
+        spot.setParkingLot(parkingLot);
+        spotRepository1.save(spot);
+        return spot;
     }
 
     @Override
     public void deleteParkingLot(int parkingLotId) {
-
+        parkingLotRepository1.deleteById(parkingLotId);
     }
 }
